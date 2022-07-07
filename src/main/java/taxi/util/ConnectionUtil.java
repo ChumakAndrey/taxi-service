1f28c8e4ed3eb29a02e68dacde827e89d17782a2
Andrey Chumak <chumakandrey7@gmail.com>
2022-07-07 16:43:54 +0300

    Changed Java version

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -3,10 +3,11 @@ package taxi.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
+    /*private static final String URL = "jdbc:mysql://localhost:3306/taxi";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "11111111";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
@@ -25,6 +26,34 @@ public class ConnectionUtil {
         dbProperties.setProperty("password", PASSWORD);
         try {
             return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB ", e);
+        }*/
+    private static final String dbServer = "mysql-82179-0.cloudclusters.net";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "lYi1XQGx";
+    private static final String dbName = "taxi-service";
+
+    private static final int dbPort = 10637;
+    private static final String URL = String.format("jdbc:mysql://%s:%d?user=%s&password=%s",
+            dbServer, dbPort, USERNAME, PASSWORD);
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL);
+            String sqlusedb = "use " + dbName;
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(sqlusedb);
+            return connection;
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB ", e);
         }
