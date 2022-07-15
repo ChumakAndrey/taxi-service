ed1fac5ea6040e9765bbeafb59fb560647cead02
Andrey Chumak <chumakandrey7@gmail.com>
2022-07-15 11:37:43 +0300

    Corrected README.md - added information about versions of some technologies and added information about project architecture.

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -27,15 +27,18 @@ public class ConnectionUtil {
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB ", e);
         }*/
-    private static final String dbServer = "mysql-82179-0.cloudclusters.net";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "lYi1XQGx";
+    private static final String dbServer = "sql11.freesqldatabase.com";
+    private static final String USERNAME = "sql11506635";
+    private static final String PASSWORD = "V5ylb1UW7T";
     private static final String dbName = "taxi-service";
 
-    private static final int dbPort = 10637;
+    private static final int dbPort = 3306;
     private static final String URL = String.format("jdbc:mysql:"
-                    + "//%s:%d?user=%s&password=%s/taxi-service",
+                    + "//%s:%d?user=%s&password=%s/sql11506635",
             dbServer, dbPort, USERNAME, PASSWORD);
+    /*private static final String URL = "jdbc:mysql://sql11.freesqldatabase.com:3306;"
+            + "database=sql11506635;user=sql11506635;password=V5ylb1UW7T;";*/
+
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
